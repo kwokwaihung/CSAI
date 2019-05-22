a729b6dc16db6e6122f91d6bb381339f09afe4cd
kwokwaihung <waikhplkcc@gmail.com>
2019-05-22 15:53:30 +0800

    Add files via upload

---
+++ executable/batch.bat
@@ -0,0 +1,8 @@
+%% Usage
+%% CSAI (name of input image) (name of output image) (no of bits) (no of noise samples used) (no of iterations to test multi-threading performance)
+%% e.g. CSAI (input image="lena_D.bmp") (output image="results\lena_D_JEI.bmp") (no of bits=24) (noise sample=8) (no of iter=1)
+%% e.g. CSAI lena_D.bmp results\lena_D_JEI.bmp 24 8 1
+
+CSAI lena_D.bmp results\lena_D_JEI.bmp 24 8 1
+
+pause
